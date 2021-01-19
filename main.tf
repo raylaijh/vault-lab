4882eb3459c405e2faaa1101ba73b7e5976dcbca
raylaijh <32132864+raylaijh@users.noreply.github.com>
2021-01-19 15:46:01 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
 #  credentials = file("/Users/raymond/Downloads/vault-setup-raymond-2097d87a40e1.json")
   credentials = var.secret
-  project = "vault-setup-raymond"
+  project = var.project
   region  = "asia-southeast1"
   zone    = "asia-southeast1-a"
 }
